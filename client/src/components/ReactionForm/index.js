f67d5f5c3e2277bf374d74e00b2c58ee1a035bbb
itzguled <itzguled55337@gmail.com>
2022-06-07 17:01:22 -0500

    fix heroku

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { ADD_REACTION } from "../../utils/mutations";
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+import { ADD_REACTION } from '../../utils/mutations';
 
 const ReactionForm = ({ thoughtId }) => {
-  const [reactionBody, setBody] = useState("");
+  const [reactionBody, setBody] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
   const [addReaction, { error }] = useMutation(ADD_REACTION);
+
+  // update state based on form input changes
   const handleChange = (event) => {
     if (event.target.value.length <= 280) {
       setBody(event.target.value);
@@ -13,6 +16,7 @@ const ReactionForm = ({ thoughtId }) => {
     }
   };
 
+  // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -20,16 +24,19 @@ const ReactionForm = ({ thoughtId }) => {
       await addReaction({
         variables: { reactionBody, thoughtId },
       });
-      setBody("");
+
+      // clear form value
+      setBody('');
       setCharacterCount(0);
     } catch (e) {
       console.error(e);
     }
   };
+
   return (
     <div>
       <p
-        className={`m-0 ${characterCount === 280 || error ? "text-error" : ""}`}
+        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
       >
         Character Count: {characterCount}/280
         {error && <span className="ml-2">Something went wrong...</span>}
@@ -49,7 +56,8 @@ const ReactionForm = ({ thoughtId }) => {
           Submit
         </button>
       </form>
-      {error && <span>Something went wrong...</span>}
+
+      {error && <div>Something went wrong...</div>}
     </div>
   );
 };
